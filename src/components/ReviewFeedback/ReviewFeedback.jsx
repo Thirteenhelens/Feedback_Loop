24dc97b99e6a9ef79eef17d1b7954315be90851a
Jack Barrett <jbarrett1347@gmail.com>
2021-10-30 13:48:15 -0500

    Restyled the reveiw page a bit

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -19,13 +19,13 @@ function ReviewFeedback() {
   return (
     <div>
       <h2>Review Your Feedback</h2>
-
-      <ul className="feedbackList">
-        <li>Feelings: {feedback.feeling}</li>
-        <li>Understanding: {feedback.understanding}</li>
-        <li>Support: {feedback.support}</li>
-        <li>Comments: {feedback.comment}</li>
-      </ul>
+      <p>Feelings: {feedback.feeling}</p>
+      <br />
+      <p>Understanding: {feedback.understanding}</p>
+      <br />
+      <p>Support: {feedback.support}</p>
+      <br />
+      <p>Comments: {feedback.comment}</p>
       <br />
       <Button onClick={handleSubmit} type="submit" variant="contained">
         Submit
