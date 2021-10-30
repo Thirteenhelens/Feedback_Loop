bcac0dfd624fa2c9fc7446df3d26fcca04f15f1c
Jack Barrett <jbarrett1347@gmail.com>
2021-10-30 13:44:38 -0500

    Added bare bones to review page and Success page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import React from "react";
-import axios from "axios";
-import { useHistory } from "react-router";
 import CommentForm from "../CommentForm/CommentForm";
 import FeelingForm from "../FeelingForm/FeelingForm";
 import SupportForm from "../SupportForm/SupportForm";
+import SuccessPage from "../SuccessPage/SuccessPage";
 import ReviewFeedback from "../ReviewFeedback/ReviewFeedback";
 import { HashRouter as Router, Route } from "react-router-dom";
 import UnderstandingForm from "../UnderstandingForm/UnderstandingForm";
 
 function App() {
-  const history = useHistory();
-
   return (
     <Router>
       <div className="App">
@@ -19,27 +16,29 @@ function App() {
           <h1 className="App-title">Feedback!</h1>
           <h4>Don't forget it!</h4>
         </header>
-
-        <Route exact path='/' >
+        <Route exact path="/">
           <FeelingForm />
         </Route>
 
-        <Route path='/understanding' >
+        <Route path="/understanding">
           <UnderstandingForm />
         </Route>
 
-        <Route path='/support' >
+        <Route path="/support">
           <SupportForm />
         </Route>
 
-        <Route path='/comment' >
+        <Route path="/comment">
           <CommentForm />
         </Route>
 
-        <Route path='/review' >
+        <Route path="/review">
           <ReviewFeedback />
         </Route>
 
+        <Route path="/success">
+          <SuccessPage />
+        </Route>
       </div>
     </Router>
   );
---
+++ src/components/SuccessPage/SuccessPage.jsx
@@ -0,0 +1,9 @@
+function SuccessPage() {
+    return(
+        <div>
+            <p>Placeholder, Fix!!!</p>
+        </div>
+    )
+}
+
+export default SuccessPage;
