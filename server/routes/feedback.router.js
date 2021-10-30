3cb34fe15d389da14627e227caaa76be6329dca7
Jack Barrett <jbarrett1347@gmail.com>
2021-10-30 12:34:27 -0500

    Added store, sorced it in, and added functionallity to feedback component

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,8 @@
+// const router = express.Router();
+// const express = require('express');
+// const pool = require('../modules/pool');
+
+// router.post('/', (req, res) => {
+//     const queryText = [req.params]
+
+// })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const placeholder = (state = { feedback: '', understanding: '', support: '', comments: '' }, action) => {
+    if (action.type === "ADD_INPUT") {
+        return action.payload
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        placeholder
+    }), applyMiddleware(
+        logger
+    )
+)
+
+ReactDOM.render(
+    <Provider store={storeInstance} >
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
 registerServiceWorker();
