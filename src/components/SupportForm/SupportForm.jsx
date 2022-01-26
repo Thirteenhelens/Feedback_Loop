af44e5f3970c52d80f615e5b017e8611949b9b68
Jack Barrett <jbarrett1347@gmail.com>
2022-01-26 14:25:45 -0600

    Added MUI Rating to SupportForm

--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import Button from "@mui/material/Button";
+
+// import TextField from "@mui/material/TextField";
+// import ArrowForwardIosTwoToneIcon from "@mui/icons-material/ArrowForwardIosTwoTone";
+// Importing everything needed.
+import React from "react";
 import { useHistory } from "react-router";
 import { useDispatch } from "react-redux";
-import TextField from "@mui/material/TextField";
-import ArrowForwardIosTwoToneIcon from "@mui/icons-material/ArrowForwardIosTwoTone";
-// Importing everything needed.
+import { Grid, Button, Rating, Typography } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function SupportForm() {
   const history = useHistory();
   const dispatch = useDispatch();
-  const [support, setSupport] = useState("");
+  const [support, setSupport] = React.useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,13 +21,37 @@ function SupportForm() {
       payload: support,
     });
 
-    setSupport("");
+    setSupport(0);
     history.push("/comment");
   };
 
   return (
-    <div>
-      <div>
+    // <div>
+    <Grid container>
+      <Grid item xs={12} sx={{ p: 2 }}>
+        <Typography variant="h3">How well are you being supported?</Typography>
+      </Grid>
+      <Grid item xs={12} sx={{ p: 2 }}>
+        <Rating
+          size="large"
+          value={support}
+          name="simple-controlled"
+          onChange={(event, newValue) => {
+            setSupport(newValue);
+            console.log(newValue);
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} sx={{ p: 2 }}>
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          endIcon={<ArrowForwardIosIcon />}
+        >
+          Next
+        </Button>
+      </Grid>
+      {/* <div>
         <h2>How well are you being supported?</h2>
       </div>
       <form onSubmit={handleSubmit}>
@@ -49,7 +75,8 @@ function SupportForm() {
           Next
         </Button>
       </form>
-    </div>
+    </div> */}
+    </Grid>
   );
 }
 
